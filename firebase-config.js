85a48da190fd0a591d36a3f747d8b0e7730e1dfa
nishida1127 <nishida.tks@gmail.com>
2025-06-22 21:00:36 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCd5dXABidFlZqypvwB8ARzOuC0BjDtB0Y",
+  authDomain: "shopping-list-eef81.firebaseapp.com",
+  databaseURL: "https://shopping-list-eef81-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shopping-list-eef81",
+  storageBucket: "shopping-list-eef81.firebasestorage.app",
+  messagingSenderId: "527901026853",
+  appId: "1:527901026853:web:8246ce69cac03f9fb6adcc",
+  measurementId: "G-G7L2VQT2N4"
+};
+firebase.initializeApp(firebaseConfig);

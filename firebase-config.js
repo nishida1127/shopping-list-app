0c5f301b26bcfdbf68baa268d010dd2a3d7ac358
nishida1127 <nishida.tks@gmail.com>
2025-06-23 22:06:11 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,9 +3,11 @@ const firebaseConfig = {
   authDomain: "shopping-list-eef81.firebaseapp.com",
   databaseURL: "https://shopping-list-eef81-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "shopping-list-eef81",
-  storageBucket: "shopping-list-eef81.firebasestorage.app",
+  storageBucket: "shopping-list-eef81.appspot.com",
   messagingSenderId: "527901026853",
   appId: "1:527901026853:web:8246ce69cac03f9fb6adcc",
   measurementId: "G-G7L2VQT2N4"
 };
+
 firebase.initializeApp(firebaseConfig);
+
